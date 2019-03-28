efebb5937eb9d47b1bbcf1ef743c5ac02bc8d1de
Robert Hardin <45836660+Rhardin94@users.noreply.github.com>
2019-03-27 21:42:16 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
+//Hides about me content until button click
+    $(".wrapper").hide();
+    //Hides contact me content until button click
+    $(".contactDiv").hide();
 //Waits for HTML to load before running anything
 $(document).ready(function() {
     //Hiding project divs at start to cycle through later
     $(".GifTastic").hide();
     $(".Office-Trivia").hide();
-    //Hides about me content until button click
-    $(".wrapper").hide();
-    //Hides contact me content until button click
-    $(".contactDiv").hide();
     //Assigning variables to each about div and hiding it for animation later
     const aboutMe = $(".aboutMe");
     aboutMe.hide();
