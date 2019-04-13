e4778dc061facb08acd1f4685d71e6a5146e4330
Robert H <robertjameshardin@aol.com>
2019-04-12 22:12:06 -0500

    Applied corrections from grading

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
 //Waits for HTML to load before running anything
 $(document).ready(function () {
-    //Hiding project divs at start to cycle through later
-    $(".GifTastic").hide();
-    $(".Office-Trivia").hide();
     //Hides about me content until button click
     $(".wrapper").hide();
     //Hides contact me content until button click
@@ -10,35 +7,35 @@ $(document).ready(function () {
     //Assigning variables to each about div and hiding it for animation later
     const aboutMe = $(".aboutMe");
     aboutMe.hide();
-    const onlineInfo = $(".onlineInfo");
-    onlineInfo.hide();
     const portfolio = $(".portfolio");
     portfolio.hide();
+    const footer = $("footer");
+    footer.hide();
     //On click that migrates HTML to about me content by hiding introDiv, showing about div (wrapper) and setting timeouts for animation
     $(".aboutBtn").on("click", function () {
         $(".introDiv").hide();
         $(".wrapper").addClass("animated fadeIn slow");
         $(".wrapper").show();
         setTimeout(aboutDiv, 1000);
-        setTimeout(contactDiv, 3000);
         setTimeout(portfolioDiv, 2000);
+        setTimeout(footerMagic, 3000);
     });
     function aboutDiv() {
         aboutMe.addClass("animated fadeInRight slow");
         aboutMe.show();
-    };
-    function contactDiv() {
-        onlineInfo.addClass("animated fadeInUp slow");
-        onlineInfo.show()
     };
     setTimeout(projectSwap, 3000);
     function portfolioDiv() {
         portfolio.addClass("animated fadeInRight slow");
         portfolio.show();
     };
+    function footerMagic() {
+        footer.addClass("animated fadeInUp slow");
+        footer.show();
+    }
     //Function that cycles through GitHub projects
     function projectSwap() {
-        const tempArray = [];
+        //const tempArray = [];
         const pastProjects = [
             liriApp = {
                 image: $("<img src='assets/images/liri.jpg'>"),
@@ -65,35 +62,49 @@ $(document).ready(function () {
                 repo: $("<a href='https://github.com/Rhardin94/Office-Trivia' target='_blank'>").html("<h3> Link to Repo! </h3>")
             }
         ];
-        let index = Math.floor(Math.random() * pastProjects.length);
-        let displayedProject = pastProjects[index]
+        /*let index = Math.floor(Math.random() * pastProjects.length);
         tempArray.push(displayedProject);
-        pastProjects.splice(index, 1);
-        const currentProject = $("<div>").attr("class", "animated fadeInRight slow");
-        $(currentProject).append(displayedProject.image);
-        $(currentProject).append(displayedProject.name);
-        $(currentProject).append(displayedProject.deployed);
-        $(currentProject).append(displayedProject.repo);
-        $(".currentProject").html(currentProject);
-        setTimeout(projectSwap, 8000);
+        pastProjects.splice(index, 1);*/
+        for (let i = 0; i < pastProjects.length; i++) {
+            let displayedProject = pastProjects[i];
+            const currentProject = $("<div>").attr("class", "animated fadeIn slow");
+            $(currentProject).append(displayedProject.image);
+            $(currentProject).append(displayedProject.name);
+            $(currentProject).append(displayedProject.deployed);
+            $(currentProject).append(displayedProject.repo);
+            $(".currentProject").append(currentProject);
+        }
     };
     //On-click that migrates HTML to contact form
     $(".contactBtn").on("click", function () {
         $(".wrapper").hide();
         $(".contactDiv").addClass("animated fadeInRight slow");
+        $(".header").hide();
         $(".contactDiv").show();
+        $(".header").addClass("animated fadeIn slow");
+        setTimeout(function() {
+            $(".header").show();
+        }, 2000);
+        $("footer").hide();
     });
     //On-click that will eventually submit info to me and also navigates back to homepage
     $(".submitBtn").on("click", function (event) {
         //Preventing button from refreshing page
         event.preventDefault();
+        if ((!$(".nameInput").val()) || (!$(".emailInput").val()) || (!$(".messageInput").val())) {
+            $("label").append(" (You didn't fill in all the required fields!)");
+            return false;
+        };
         //Something magic happens that sends me this info
         //At the moment, it clears the input fields and returns user to about page
         $(".contactDiv").hide();
+        setTimeout(function() {
         $(".nameInput").val("");
         $(".emailInput").val("");
         $(".messageInput").val("");
+        },3000);
         $(".wrapper").show();
+        $("footer").show();
     })
     //On-Click to navigate away from contact page
     $(".homeBtn").on("click", function (event) {
@@ -105,5 +116,6 @@ $(document).ready(function () {
         $(".emailInput").val("");
         $(".messageInput").val("");
         $(".wrapper").show();
+        $("footer").show();
     });
 });
