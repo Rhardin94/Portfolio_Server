c667e211abfc4316fa68fb22455c2f1032cedbb1
Robert H <robertjameshardin@aol.com>
2019-06-30 21:13:35 -0500

    successfully receive nodemailer post request without page reload

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const mailer = require("nodemailer").createTransport({
     pass: process.env.GMAIL_PASS
   }
 });
-console.log(process.env.GMAIL_ADDRESS);
-console.log(process.env.GMAIL_PASS);
+// console.log(process.env.GMAIL_ADDRESS);
+// console.log(process.env.GMAIL_PASS);
 //Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -23,14 +23,17 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "home.html"));
 });
 app.post("/contact", (req, res) => {
+  //CORS solution found here: https://stackoverflow.com/questions/47523265/jquery-ajax-no-access-control-allow-origin-header-is-present-on-the-requested/47525511
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   mailer.sendMail({
     from: req.body.Email,
     to: [CONTACT_ADDRESS],
     subject: "Nodemailer test",
-    text: req.body.Message || "[No Message]",
+    text: `From: ${req.body.Email}` + "\n" + `Message:  ${req.body.Message}` || "[No Message]",
   }, (err, info) => {
     if (err) return res.status(500).end();
-    res.status(200).end();
+    res.send(200).end();
     console.log(info);
     });
 });
