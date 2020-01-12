35c705bd81fd692f96a5236ccc98be4c55063061
Darkclyde <robertjameshardin@aol.com>
2020-01-12 14:21:02 -0600

    Revert "still troubleshooting server error for post request, have to deploy to heroku to test solution, as it works locally"
    This reverts commit ad1329e0bc0ee3a378f4836cbe11844b8985f9f0.

--- server.js
+++ server.js
@@ -15,9 +15,7 @@ const mailer = require("nodemailer").createTransport({
 // console.log(process.env.GMAIL_ADDRESS);
 // console.log(process.env.GMAIL_PASS);
 //Middleware
-app.use(express.urlencoded({
-  extended: false
-}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));  
 //Routing
@@ -30,21 +28,21 @@ app.get("/wake", (req, res) => {
   res.send("I'm awake!").end();
 });
 app.post("/contact", (req, res) => {
+  //CORS solution found here: https://stackoverflow.com/questions/47523265/jquery-ajax-no-access-control-allow-origin-header-is-present-on-the-requested/47525511
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   if ((!req.body.Email) || (!req.body.Message)) {
     return res.status(404).end();
   } else {
-    mailer.sendMail({
-      from: req.body.Email,
-      to: [CONTACT_ADDRESS],
-      subject: req.body.Subject,
-      text: `From: ${req.body.Email}` + "\n" + `Message:  ${req.body.Message}` || "[No Message]",
-    }, (err, info) => {
-      if (err) return res.status(500).end();
-      //CORS solution found here: https://stackoverflow.com/questions/47523265/jquery-ajax-no-access-control-allow-origin-header-is-present-on-the-requested/47525511
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      res.send("Email went through!").end();
-      console.log(info);
+  mailer.sendMail({
+    from: req.body.Email,
+    to: [CONTACT_ADDRESS],
+    subject: req.body.Subject,
+    text: `From: ${req.body.Email}` + "\n" + `Message:  ${req.body.Message}` || "[No Message]",
+  }, (err, info) => {
+    if (err) return res.status(500).end();
+    res.send("Email went through!").end();
+    console.log(info);
     });
   }
 });
